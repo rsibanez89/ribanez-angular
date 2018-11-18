a18da2f5eee61f9d0c6ef16a919916a97ffdaa00
rodrigo.ibanez <rodrigo.ibanez@rokt.com>
2018-11-18 17:10:45 +1100

    Added home description

--- src/app/site/site.module.ts
+++ src/app/site/site.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { BlogComponent } from './blog/blog.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgZorroAntdModule
   ],
   declarations: [
     HomeComponent,
